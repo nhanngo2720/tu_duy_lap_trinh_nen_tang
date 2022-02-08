aaf69c98c126bd15f155858e22f6a6b4892e746c
nhanngo2720 <60211514+nhanngo2720@users.noreply.github.com>
2022-02-08 10:41:54 +0900

    update input field

--- App.java
+++ App.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class App {
 
 public static int maxValue(int a ,int  b , int c ) {
+    // dung int la de check so nguyen
         int maxValue = a;
          if (b> a) {
              maxValue = b;
@@ -11,9 +14,21 @@ public static int maxValue(int a ,int  b , int c ) {
          return maxValue; 
      }
 public static void main(String[] args){
-    int x = 1;
-    int y = 2;
-    int z = 3;
+    //day la so cho san 
+    // int x = 1;
+    // int y = 2;
+    // int z = 3;
+    // tao input cho nguoi dung
+Scanner scan = new Scanner(System.in);
+//xuat thong bao nhap vao 3 so
+System.out.println("MaxValue finder");
+System.out.println("input your 1st number");
+int x = Integer.parseInt(scan.nextLine());
+System.out.println("input your 2nd number");
+int y = Integer.parseInt(scan.nextLine());
+System.out.println("input your 3rd number");
+int z = Integer.parseInt(scan.nextLine());
+
 int result = maxValue(x,y,z);
 System.out.println("so lon nhat is: " + result); 
     }
