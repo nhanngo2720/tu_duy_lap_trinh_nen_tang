eaaf9b9b85277002a67005a0e5bf57ae28499b0e
nhanngo2720 <60211514+nhanngo2720@users.noreply.github.com>
2022-02-09 22:38:24 +0900

    update

--- Bai_30_tinh_tien_grab/grabTinhTien/src/App.java
+++ Bai_30_tinh_tien_grab/grabTinhTien/src/App.java
@@ -7,34 +7,68 @@ public class App {
 //dang ket o cho khong biet goi ham loai xe tuong ung
 
 //thoi gian cho
-    public static int tgianCho(int a){
-        return a/3;
-    };
+    //public static int tgianCho(int a){
+    //     return a/3;
+    // };
 
-//loai GrabCar
-    public static int loaiXe(int a){
-        //truong hop di duoi 19km 
-        if(a <= 19){
-            int thanhTien = ( 8000 + (a - 1)*7500 );
-        //truong hop di tren 19km     
-        }if(a >19){ 
-            int thanhTien = ( 8000 + 18*7500 + (a-19)*7000 );
-        }
-        return thanhTien;
+// //loai GrabCar
+//     public static int loaiXe(int a){
+//         //truong hop di duoi 19km 
+//         if(a <= 19){
+//             int thanhTien = ( 8000 + (a - 1)*7500 );
+//         //truong hop di tren 19km     
+//         }if(a >19){ 
+//             int thanhTien = ( 8000 + 18*7500 + (a-19)*7000 );
+//         }
+//         return thanhTien;
+//     }
+//don gia grabCar
+static final int carFirst = 8000;
+static final int car1to19 = 7500;
+static final int carFrom19 = 7000;
+static final int carwair = 2000;
+//don gia grab SUV
+static final int suvFirst = 9000;
+static final int suv1to19 = 8500;
+static final int suvFrom19 = 8000;
+static final int suvWait = 3000;
 
-    }
+//don gia grab Black
+static final int blackFirst = 8000;
+static final int black1to19 = 7500;
+static final int blackFrom19 = 7000;
+static final int blackWait = 3500;
+
+// ham tinh firstKM
+
+public static firstKM( ) {
+    int km;   
+}
+// ham chon loai xe
+public static selectCar(){
+    int type;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Vui long chon loai xe");
+    System.out.println("1 GrabCar");
+    System.out.println("2 SUV");
+    System.out.println("3 Black");
+    //cho chon loai grab ne
+   type = Integer.parseInt(scan.nextLine());
+   return type ;
+
+
+}
+
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         System.out.println("Chuong trinh tinh so tien xe");
-        System.out.println("vui long nhap nhap loai xe");
-        int x = Integer.parseInt(scan.nextLine());
         System.out.println("Vui long nhap quang duong di duoc");
         int y = Integer.parseInt(scan.nextLine());
         System.out.println("Vui long nhap thoi gian cho");
         int z = Integer.parseInt(scan.nextLine());
-        int tongTien = loaiXe(y)+ tgianCho(z);
-        System.out.println("tong tien phai tra la " + tongTien);
+        // int tongTien = loaiXe(y)+ tgianCho(z);
+        // System.out.println("tong tien phai tra la " + tongTien);
 
     }
 }
---
+++ Bai_30_tinh_tien_grab/grabTinhTien/src/thayGiai.java
@@ -0,0 +1,8 @@
+
+
+public class thayGiai {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hell");
+    }
+    
+}
